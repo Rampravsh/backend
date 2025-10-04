1c25b36cc6aa85b48a0e8c39ebeedfe13123dbef
rampravesh <rampraveshkr4545@gmail.com>
2025-10-04 17:00:59 +0530

    Update .gitignore and add dotenv dependency for environment variable management

--- deploy_backend/index.js
+++ deploy_backend/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = 5000 || process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("hello World!");
 });
-app.get('/twitter',(req,res)=>{
-    res.send("twitter")
-})
+app.get("/twitter", (req, res) => {
+  res.send("twitter");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>please login at chai aur code</h1>");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
