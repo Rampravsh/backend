444663c0daedfcb98b1075bed4ac1196e06ab54d
Rampravesh <rampraveshkr4545@gmail.com>
2025-11-04 19:12:50 +0530

    feat : add file and conect  to mongo atlas

--- backend_project/src/index.js
+++ backend_project/src/index.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+dotenv.config();
+const PORT = process.env.PORT;
+connectDB();
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("error: ", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is running on port : ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("error:", error);
+//   }
+// };
+// connectDB();
